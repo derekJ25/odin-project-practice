c849b8c2853c4e9b35f49854fe8c55cffab25d8a
Derek J <derekjiang25@gmail.com>
2023-11-22 11:05:59 +1100

    fixed borders of generated grid

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,21 +1,35 @@
 const GRID_COLUMN = 16;
 const GRID_ROW = 16;
+const LAST_GRID_COLUMN = GRID_COLUMN - 1;
+const LAST_GRID_ROW = GRID_ROW - 1;
 
 const container = document.querySelector(".container");
 
 const generateGrid = () => {
-  for (let row = 0; row < GRID_ROW; row++) {
+  for (let column = 0; column < GRID_COLUMN; column++) {
     let gridRow = document.createElement("div");
     gridRow.className = "grid-row";
+    if (column == LAST_GRID_COLUMN) {
+      gridRow.style = "border-right: 1px solid black";
+    }
 
-    for (let column = 0; column < GRID_COLUMN; column++) {
-      let gridColumn = document.createElement("div");
-      gridColumn.className = "grid-item";
-      gridColumn.textContent = "o";
-      gridRow.appendChild(gridColumn);
+    for (let row = 0; row < GRID_ROW; row++) {
+      let gridCell = document.createElement("div");
+      gridCell.className = "grid-item neutral";
+      if (row == LAST_GRID_ROW) {
+        gridCell.style = "border-bottom: 1px solid black";
+      }
+      //   gridCell.addEventListener("click", flipCell);
+      gridRow.appendChild(gridCell);
       container.appendChild(gridRow);
     }
   }
 };
+
+// const flipCell = () => {
+//   const cell = document.querySelector(".grid-item");
+//   cell.className = "grid-item inverse";
+//   console.log("flip cell");
+// };
 
 generateGrid();
