75cf283e8a5cb748274d64cccfcdc2bcf059dfdb
Derek J <derekjiang25@gmail.com>
2023-11-24 11:57:35 +1100

    working on button to ask user for rows and columns

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -2,8 +2,32 @@ const GRID_COLUMN = 16;
 const GRID_ROW = 16;
 const LAST_GRID_COLUMN = GRID_COLUMN - 1;
 const LAST_GRID_ROW = GRID_ROW - 1;
+const DELIMITER = " ";
+const EXPECTED_ROW_COL_VALUES = 2;
 
 const container = document.querySelector(".container");
+
+const askUserAmountOfGrid = () => {
+  const gridSize = prompt("Enter column and row size: (x y)", "3 5");
+  if (gridSize == null || gridSize == "") {
+    console.log("Cancelled");
+  } else {
+    const size = gridSize.trim().split(DELIMITER);
+    if (size.length == EXPECTED_ROW_COL_VALUES) {
+      // upto: test if two values are both numbers
+    } else {
+      console.log("Invalid size input. Please try again.");
+    }
+  }
+
+  // check if empty or null - user cancelled
+  // check if values are greater than 100 or below 0
+  // check if two values are numbers
+  // check if input format is correct
+  // check only containers numbers and a whitespace
+
+  // if values are valid, generate grid
+};
 
 const generateGrid = () => {
   for (let column = 0; column < GRID_COLUMN; column++) {
@@ -33,5 +57,11 @@ const generateGrid = () => {
     }
   }
 };
+
+const button = document.createElement("button");
+button.textContent = "Click here ";
+button.classList.add("noOfGrid");
+button.addEventListener("click", askUserAmountOfGrid);
+container.appendChild(button);
 
 generateGrid();
