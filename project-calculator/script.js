036693ecc7fb313be1775df879c1db6175045aed
Derek J <derekjiang25@gmail.com>
2023-12-04 10:46:56 +1100

    fixed operate function and added result to the display header

--- project-calculator/script.js
+++ project-calculator/script.js
@@ -22,61 +22,49 @@ const operate = (operator, a, b) => {
   } else {
     const operatorIndex = operators.indexOf(operator);
     if (operatorIndex == 0) {
-      add(a, b);
+      return add(a, b).toFixed(5);
     } else if (operatorIndex == 1) {
-      subtract(a, b);
+      return subtract(a, b).toFixed(5);
     } else if (operatorIndex == 2) {
-      multiply(a, b);
+      return multiply(a, b).toFixed(5);
     } else {
-      divide(a, b);
+      return divide(a, b).toFixed(5);
     }
   }
 };
 
-// let displayValue = document.querySelector("span");
-// const buttons = document.querySelectorAll("button");
-// buttons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     if (button.id == "clear") {
-//       displayValue.innerHTML = "";
-//     } else if (button.id == "total") {
-//       getDisplayEquation();
-//     } else {
-//       displayValue.innerHTML += button.innerHTML;
-//     }
-//   });
-// });
+let displayValue = document.querySelector("span");
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.id == "clear") {
+      displayValue.innerHTML = "";
+    } else if (button.id == "total") {
+      getDisplayEquation();
+    } else {
+      displayValue.innerHTML += button.innerHTML;
+    }
+  });
+});
 
-// const getDisplayEquation = () => {
-//   const displayEquation = displayValue.innerHTML;
-//   let equationOperator;
-//   let foundOperator = false;
+const getDisplayEquation = () => {
+  let displayEquation = displayValue.innerHTML;
+  let equationOperator;
+  let foundOperator = false;
 
-//   for (let i = 0; i < operators.length; i++) {
-//     if (displayEquation.includes(operators[i])) {
-//       equationOperator = operators[i];
-//       foundOperator = true;
-//     }
-//   }
+  for (let i = 0; i < operators.length; i++) {
+    if (displayEquation.includes(operators[i])) {
+      equationOperator = operators[i];
+      foundOperator = true;
+    }
+  }
 
-//   if (foundOperator) {
-//     console.log("found operator");
-//     const equation = displayEquation.split(equationOperator);
-//     console.log(
-//       `operator: ${equationOperator} | value1: ${equation[0]} | value2: ${equation[1]} `
-//     );
-
-//     // console.log();
-//     // operate(equationOperator, equation[0], equation[1]);
-//     // console.log(operate(equationOperator, equation[0], equation[1]));
-//   }
-
-//   return "temp";
-// };
-
-const a = 9;
-const b = 3;
-const operator = "+";
-
-console.log(operate(operator, a, b));
-console.log(operate("9", a, b));
+  if (foundOperator) {
+    const equation = displayEquation.split(equationOperator);
+    displayValue.innerHTML = operate(
+      equationOperator,
+      +equation[0],
+      +equation[1]
+    );
+  }
+};
