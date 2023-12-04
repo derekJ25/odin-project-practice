11876a9e68d5cf066ea0a809b72d288451f56227
Derek J <derekjiang25@gmail.com>
2023-12-04 11:34:32 +1100

    added test cases

--- project-calculator/script.js
+++ project-calculator/script.js
@@ -22,13 +22,13 @@ const operate = (operator, a, b) => {
   } else {
     const operatorIndex = operators.indexOf(operator);
     if (operatorIndex == 0) {
-      return add(a, b).toFixed(5);
+      return add(a, b);
     } else if (operatorIndex == 1) {
-      return subtract(a, b).toFixed(5);
+      return subtract(a, b);
     } else if (operatorIndex == 2) {
-      return multiply(a, b).toFixed(5);
+      return multiply(a, b);
     } else {
-      return divide(a, b).toFixed(5);
+      return divide(a, b);
     }
   }
 };
@@ -47,24 +47,81 @@ buttons.forEach((button) => {
   });
 });
 
-const getDisplayEquation = () => {
-  let displayEquation = displayValue.innerHTML;
-  let equationOperator;
-  let foundOperator = false;
+// const getDisplayEquation = () => {
+//   let displayEquation = displayValue.innerHTML;
+//   let equationOperator;
+//   let foundOperator = false;
 
-  for (let i = 0; i < operators.length; i++) {
-    if (displayEquation.includes(operators[i])) {
-      equationOperator = operators[i];
-      foundOperator = true;
-    }
-  }
+//   for (let i = 0; i < operators.length; i++) {
+//     if (displayEquation.includes(operators[i])) {
+//       equationOperator = operators[i];
+//       foundOperator = true;
+//     }
+//   }
 
-  if (foundOperator) {
-    const equation = displayEquation.split(equationOperator);
-    displayValue.innerHTML = operate(
-      equationOperator,
-      +equation[0],
-      +equation[1]
-    );
-  }
-};
+//   if (foundOperator) {
+//     const equation = displayEquation.split(equationOperator);
+//     displayValue.innerHTML = operate(
+//       equationOperator,
+//       +equation[0],
+//       +equation[1]
+//     );
+//   }
+// };
+
+console.log("TEST CASES");
+const a = 9;
+const b = 3;
+const c = 4.8;
+const d = 2.1;
+const zero = 0;
+
+const testCasesAddition = [
+  { name: `addition = (${a}+${b})`, result: operate("+", a, b) },
+  { name: `addition = (-${a}+${b})`, result: operate("+", -a, b) },
+  { name: `addition = (${a}+-${b})`, result: operate("+", a, -b) },
+  { name: `addition = (-${a}+-${b})`, result: operate("+", -a, -b) },
+  { name: `addition = (${a}++${b})`, result: operate("+", a, b) },
+  { name: `addition = (${c}+${d})`, result: operate("+", c, d) },
+  { name: `addition = (${c}+++${d})`, result: operate("++", c, d) },
+];
+
+const testCasesSubtraction = [
+  { name: `subtraction = (${a}-${b})`, result: operate("-", a, b) },
+  { name: `subtraction = (-${a}-${b})`, result: operate("-", -a, b) },
+  { name: `subtraction = (${a}--${b})`, result: operate("-", a, -b) },
+  { name: `subtraction = (-${a}--${b})`, result: operate("-", -a, -b) },
+  { name: `subtraction = (${c}-${d})`, result: operate("-", c, d) },
+  { name: `subtraction = (${a}---${b})`, result: operate("--", a, -b) },
+];
+
+const testCasesMultiply = [
+  { name: `multiply = (${zero}*${b})`, result: operate("*", zero, b) },
+  { name: `multiply = (${a}*${b})`, result: operate("*", a, b) },
+  { name: `multiply = (-${a}*${b})`, result: operate("*", -a, b) },
+  { name: `multiply = (${a}*-${b})`, result: operate("*", a, -b) },
+  { name: `multiply = (-${a}*-${b})`, result: operate("*", -a, -b) },
+  { name: `multiply = (${a}**${b})`, result: operate("**", a, b) },
+  { name: `multiply = (${c}*${d})`, result: operate("*", c, d) },
+];
+
+const testCasesDivide = [
+  { name: `divide = (${a}/${b})`, result: operate("/", a, b) },
+  { name: `divide = (-${a}/${b})`, result: operate("/", -a, b) },
+  { name: `divide = (${a}/-${b})`, result: operate("/", a, -b) },
+  { name: `divide = (${zero}/${b})`, result: operate("/", zero, b) },
+  { name: `divide = (${a}/${zero})`, result: operate("/", a, zero) },
+  { name: `divide = (-${a}/-${b})`, result: operate("/", -a, -b) },
+  { name: `divide = (${a}//${b})`, result: operate("//", a, b) },
+  { name: `divide = (${c}/${d})`, result: operate("/", c, d) },
+];
+
+console.table(testCasesAddition);
+console.table(testCasesSubtraction);
+console.table(testCasesMultiply);
+console.table(testCasesDivide);
+
+// Complex test cases
+// 1 + 2 - 3
+// 1 + 2 * 3
+// 1 + 0 * 3
