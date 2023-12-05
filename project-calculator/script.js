e390391972b2f46e010d63fe834d4b55a9d21277
Derek J <derekjiang25@gmail.com>
2023-12-05 14:12:41 +1100

    changed the UX of the calculator

--- project-calculator/script.js
+++ project-calculator/script.js
@@ -1,51 +1,71 @@
 const operators = ["+", "-", "*", "/"];
 
-const add = (a, b) => {
-  return a + b;
-};
-
-const subtract = (a, b) => {
-  return a - b;
-};
-
-const multiply = (a, b) => {
-  return a * b;
-};
-
-const divide = (a, b) => {
-  return a / b;
-};
-
-const operate = (operator, a, b) => {
-  if (!operators.includes(operator)) {
-    console.log("operator not found");
-  } else {
-    const operatorIndex = operators.indexOf(operator);
-    if (operatorIndex == 0) {
-      return add(a, b);
-    } else if (operatorIndex == 1) {
-      return subtract(a, b);
-    } else if (operatorIndex == 2) {
-      return multiply(a, b);
-    } else {
-      return divide(a, b);
-    }
-  }
-};
-
-let displayValue = document.querySelector("span");
+let total = 0;
+let value1 = null;
+let value2 = null;
+let operator1 = null;
+let operator2 = null;
 const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (button.id == "clear") {
-      displayValue.innerHTML = "";
-    } else if (button.id == "total") {
-      getDisplayEquation();
-    } else {
-      displayValue.innerHTML += button.innerHTML;
-    }
-  });
-});
+let displayValue = document.querySelector("span");
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// const subtract = (a, b) => {
+//   return a - b;
+// };
+
+// const multiply = (a, b) => {
+//   return a * b;
+// };
+
+// const divide = (a, b) => {
+//   return a / b;
+// };
+
+// const operate = (operator, a, b) => {
+//   if (!operators.includes(operator)) {
+//     console.log("operator not found");
+//   } else {
+//     const operatorIndex = operators.indexOf(operator);
+//     if (operatorIndex == 0) {
+//       return add(a, b);
+//     } else if (operatorIndex == 1) {
+//       return subtract(a, b);
+//     } else if (operatorIndex == 2) {
+//       return multiply(a, b);
+//     } else {
+//       return divide(a, b);
+//     }
+//   }
+// };
+
+// const updateDisplay = () => {
+//   buttons.forEach((button) => {
+//     button.addEventListener("click", () => {
+//       if (button.id == "clear") {
+//         displayValue.innerHTML = "";
+//       } else if (button.id == "total") {
+//         getDisplayEquation();
+//       } else {
+//         displayValue.innerHTML += button.innerHTML;
+//       }
+//     });
+//   });
+// };
+
+//   buttons.forEach((button) => {
+//     button.addEventListener("click", () => {
+//       if (button.id == "clear") {
+//         displayValue.innerHTML = "";
+//       } else if (button.id == "total") {
+//         getDisplayEquation();
+//       } else {
+//         displayValue.innerHTML += button.innerHTML;
+//       }
+//     });
+//   });
 
 // const getDisplayEquation = () => {
 //   let displayEquation = displayValue.innerHTML;
@@ -84,6 +104,7 @@ const testCasesAddition = [
   { name: `addition = (${a}++${b})`, result: operate("+", a, b) },
   { name: `addition = (${c}+${d})`, result: operate("+", c, d) },
   { name: `addition = (${c}+++${d})`, result: operate("++", c, d) },
+  { name: `addition = (+${a}+${b})`, result: operate("+", a, b) },
 ];
 
 const testCasesSubtraction = [
