d1d65d45fc4886b3963cdeee585d6a87ded3d581
Derek J <derekjiang25@gmail.com>
2024-02-14 19:39:17 +1100

    Immediantly Invoked Function Expression (IIFE) example

--- javascript-course/basics/factory-functions-module/factory-functions.js
+++ javascript-course/basics/factory-functions-module/factory-functions.js
@@ -1,19 +1,42 @@
-function createUser(name) {
-  const discordName = "@" + name;
+// function createUser(name) {
+//   const discordName = "@" + name;
 
-  let reputation = 0;
-  const getReputation = () => reputation;
-  const giveReputation = () => reputation++;
+//   let reputation = 0;
+//   const getReputation = () => reputation;
+//   const giveReputation = () => reputation++;
 
-  return { name, discordName, getReputation, giveReputation };
-}
+//   return { name, discordName, getReputation, giveReputation };
+// }
 
-const josh = createUser("josh");
-josh.giveReputation();
-josh.giveReputation();
+// function createPlayer(name, level) {
+//   const { getReputation, giveReputation } = createUser(name);
 
-console.log({
-  discordName: josh.discordName,
-  reputation: josh.getReputation(),
-});
-// logs { discordName: "@josh", reputation: 2 }
+//   const increaseLevel = () => level++;
+//   return { name, getReputation, giveReputation, increaseLevel };
+// }
+
+// const josh = createUser("josh");
+// josh.giveReputation();
+// josh.giveReputation();
+
+// console.log({
+//   discordName: josh.discordName,
+//   reputation: josh.getReputation(),
+// });
+// // logs { discordName: "@josh", reputation: 2 }
+
+// const player = createPlayer("player", 2);
+// console.log(player);
+
+// Calculator example
+const calculator = (function () {
+  const add = (a, b) => a + b;
+  const sub = (a, b) => a - b;
+  const mul = (a, b) => a * b;
+  const div = (a, b) => a / b;
+  return { add, sub, mul, div };
+})();
+
+console.log(calculator.add(3, 5)); // 8
+console.log(calculator.sub(6, 2)); // 4
+console.log(calculator.mul(14, 5534)); // 77476
