ff27502fe7fccc92122430ea2a2060efdf6fb262
Derek J <derekjiang25@gmail.com>
2024-02-16 10:27:04 +1100

    added example of arrays objects and primatives

--- javascript-course/basics/factory-functions-module/module-pattern.js
+++ javascript-course/basics/factory-functions-module/module-pattern.js
@@ -1,16 +1,51 @@
-const Formatter = (function () {
-  const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
+// Source/Credit: https://dev.to/tomekbuszewski/module-pattern-in-javascript-56jm
 
-  // this is essentially a IIFE? instantly invoked function expression as it immidiently returns an object
+// const Formatter = (function () {
+//   const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
+
+//   // this is essentially a IIFE? instantly invoked function expression as it immidiently returns an object
+//   const makeUppercase = (text) => {
+//     log("Making uppercase");
+//     return text.toUpperCase();
+//   };
+
+//   // Without this return, the makeUppercase const cannot be used outside the scope, keeps variables private
+//   return {
+//     makeUppercase,
+//   };
+// })();
+
+// console.log(Formatter.makeUppercase("hello"));
+
+// Example with Arrays, Objects and Primatives
+const Formatter = (function () {
+  // let timesRun = 0;
+
+  const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
+  const timesRun = [];
+
+  // const setTimesRun = () => {
+  //   log("Setting times run");
+  //   ++timesRun;
+  // };
+
   const makeUppercase = (text) => {
     log("Making uppercase");
+    // setTimesRun();
+    timesRun.push(null);
     return text.toUpperCase();
   };
 
-  // Without this return, the makeUppercase const cannot be used outside the scope, keeps variables private
   return {
     makeUppercase,
+    timesRun,
   };
 })();
 
-console.log(Formatter.makeUppercase("hello"));
+console.log(Formatter.makeUppercase("tomek"));
+console.log(Formatter.makeUppercase("tomek"));
+console.log(Formatter.makeUppercase("tomek"));
+// Value can be changed as it can still be accessed outside of the scope, better to have variable to be private
+// Formatter.timesRun = 10;
+// console.log(Formatter.timesRun);
+console.log(Formatter.timesRun.length);
