a5549ebafcc2e46f40087b3746cb001a9c001190
Derek J <derekjiang25@gmail.com>
2024-02-16 11:13:32 +1100

    added examples

--- javascript-course/basics/factory-functions-module/module-pattern.js
+++ javascript-course/basics/factory-functions-module/module-pattern.js
@@ -18,34 +18,65 @@
 // console.log(Formatter.makeUppercase("hello"));
 
 // Example with Arrays, Objects and Primatives
+// const Formatter = (function () {
+//   // let timesRun = 0;
+
+//   const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
+//   const timesRun = [];
+
+//   // const setTimesRun = () => {
+//   //   log("Setting times run");
+//   //   ++timesRun;
+//   // };
+
+//   const makeUppercase = (text) => {
+//     log("Making uppercase");
+//     // setTimesRun();
+//     timesRun.push(null);
+//     return text.toUpperCase();
+//   };
+
+//   return {
+//     makeUppercase,
+//     timesRun,
+//   };
+// })();
+
+// console.log(Formatter.makeUppercase("tomek"));
+// console.log(Formatter.makeUppercase("tomek"));
+// console.log(Formatter.makeUppercase("tomek"));
+// // Value can be changed as it can still be accessed outside of the scope, better to have variable to be private
+// // Formatter.timesRun = 10;
+// // console.log(Formatter.timesRun);
+// console.log(Formatter.timesRun.length);
+
+// Examples of declaring module dependancies
+// Inject mock example
+const documentMock = (() => ({
+  querySelector: (selector) => ({
+    innerHTML: null,
+  }),
+}))();
+
 const Formatter = (function () {
-  // let timesRun = 0;
-
   const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
-  const timesRun = [];
-
-  // const setTimesRun = () => {
-  //   log("Setting times run");
-  //   ++timesRun;
-  // };
 
   const makeUppercase = (text) => {
     log("Making uppercase");
-    // setTimesRun();
-    timesRun.push(null);
     return text.toUpperCase();
+  };
+
+  const writeToDOM = (selector, message) => {
+    // document.querySelector(selector).innerHTML = message;
+    if (!!doc && "querySelector" in doc) {
+      doc.querySelector(selector).innerHTML = message;
+    }
   };
 
   return {
     makeUppercase,
-    timesRun,
+    writeToDOM,
   };
-})();
+})(document || documentMock);
 
-console.log(Formatter.makeUppercase("tomek"));
-console.log(Formatter.makeUppercase("tomek"));
-console.log(Formatter.makeUppercase("tomek"));
-// Value can be changed as it can still be accessed outside of the scope, better to have variable to be private
-// Formatter.timesRun = 10;
-// console.log(Formatter.timesRun);
-console.log(Formatter.timesRun.length);
+Formatter.writeToDOM("#target", "Hi there");
