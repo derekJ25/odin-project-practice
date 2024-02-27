a8699cb2eb55496840974da23d34c89740ecb4fb
Derek J <derekjiang25@gmail.com>
2024-02-27 19:17:15 +1100

    added rdisplay and check if game is completed logic

--- javascript-course/basics/tic-tac-toe/tic-tac-toe.js
+++ javascript-course/basics/tic-tac-toe/tic-tac-toe.js
@@ -1,11 +1,21 @@
-// const playerOneNameInput = document.querySelector("#playerOneName");
-// const playerTwoNameInput = document.querySelector("#playerTwoName");
+// Credit to Dors Coding School - https://www.youtube.com/watch?v=kVE4xX-OkJo
+// From learning and going through this video allowed me to have an understanding of IIFE module pattern and factory
+
 const startGameButton = document.querySelector("#start-game");
 const restartGameButton = document.querySelector("#restart-game");
-// const validPlayerName = /^[a-zA-Z]+$/;
 
 const PLAYER_ONE_MARKER = "x";
 const PLAYER_TWO_MARKER = "o";
+const GAMEBOARD_SIZE = 9;
+const GAMEBOARD_EMPTY = "";
+
+const displayController = (() => {
+  const renderMessage = (message) => {
+    document.querySelector(".message").innerHTML = message;
+  };
+
+  return { renderMessage };
+})();
 
 const Gameboard = (function () {
   let gameboard = ["", "", "", "", "", "", "", "", ""];
@@ -62,6 +72,9 @@ const Game = (() => {
   };
 
   const handleClick = (event) => {
+    if (gameOver) {
+      return;
+    }
     let index = parseInt(event.target.id.split("-")[1]);
 
     if (Gameboard.getGameboard()[index] !== "") {
@@ -69,125 +82,70 @@ const Game = (() => {
     }
 
     Gameboard.update(index, players[currentPlayerIndex].marker);
+
+    if (
+      checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].marker)
+    ) {
+      gameOver = true;
+      displayController.renderMessage(
+        `${players[currentPlayerIndex].name} wins!`
+      );
+    } else if (checkForTie(Gameboard.getGameboard())) {
+      gameOver = true;
+      displayController.renderMessage("It's a tie!");
+    }
     currentPlayerIndex = currentPlayerIndex == 0 ? 1 : 0;
+  };
+
+  const restart = () => {
+    for (let i = 0; i < GAMEBOARD_SIZE; i++) {
+      Gameboard.update(i, GAMEBOARD_EMPTY);
+    }
+    Gameboard.render();
+    gameOver = false;
+    document.querySelector(".message").innerHTML = "";
   };
 
   return {
     start,
     handleClick,
+    restart,
   };
 })();
+
+// 0|1|2
+// 3|4|5
+// 6|7|8
+
+function checkForWin(board) {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (board[a] && board[a] == board[b] && board[a] == board[c]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkForTie(board) {
+  return board.every((cell) => cell != "");
+}
 
 startGameButton.addEventListener("click", () => {
   Game.start();
 });
 
-// // Game logic
-// // Have variables for: Game count, player1 and player 2
-// //
-// // Start game
-// // Clear game
-// // Place marker
-// // Check if market can be placed
-// // Check if the game is won
-// // Display the board
-
-// const Game = (function () {
-//   let boardCount = 0;
-//   let board = boardGrid;
-//   let gameCount = 1;
-//   let playerOne = null;
-//   let playerTwo = null;
-
-//   const startGame = () => {
-//     console.log(`Starting game ${gameCount}!`);
-//     if (
-//       (playerOne == null && playerTwo == null) ||
-//       (playerOne.name == "" && playerTwo.name == "")
-//     ) {
-//       playerOne = createPlayer(playerOneNameInput.value, PLAYER_ONE_MARKER);
-//       playerTwo = createPlayer(playerTwoNameInput.value, PLAYER_TWO_MARKER);
-//     } else {
-//       alert("There is already a game going on!");
-//     }
-
-//     showGameInfo();
-
-//     // while(!isGameFinished()){
-
-//     // }
-//   };
-
-//   const clearGame = () => {
-//     console.log("clearing board.");
-//     playerOne.name = "";
-//     playerTwo.name = "";
-//     board = boardGrid;
-//     boardCount = 0;
-//     gameCount = 1;
-//     showGameInfo();
-//   };
-
-//   const placeMarker = () => {
-//     console.log("placing marker");
-//   };
-
-//   const markerAllowed = () => {
-//     console.log("checking if marker can be placed");
-//   };
-
-//   const isGameFinished = () => {
-//     console.log("checks if game is finished");
-//     if (boardCount < 10 || checkIfWinner()) {
-//     } else if (boardCount == 9 && checkIfWinner() == "none") {
-//     }
-//   };
-
-//   const checkIfWinner = () => {
-//     // let winner = "none";
-//     // if(){
-//     //   winner = playerOne.name;
-//     // } else if(){
-//     //   winner = playerTwo.name;
-//     // }
-//     // return winner;
-//   };
-
-//   const showGameInfo = () => {
-//     console.log("-----------------------");
-//     console.log(`P1: ${playerOne.name} | marker: ${playerOne.marker}`);
-//     console.log(`P2: ${playerTwo.name} | marker: ${playerTwo.marker}`);
-//     console.log(`Board: ${board}`);
-//     console.log(`Board count: ${boardCount}`);
-//     console.log(`Game count: ${gameCount}`);
-//     console.log("-----------------------");
-//   };
-
-//   return {
-//     startGame,
-//     clearGame,
-//   };
-// })();
-
-// const Player = function (name, marker) {
-//   this.name = name;
-//   this.marker = marker;
-// };
-
-// function createPlayer(name, marker) {
-//   return { name, marker };
-// }
-
-// const validPlayerNames = function () {
-//   if (
-//     validPlayerName.exec(playerOneNameInput.value) &&
-//     validPlayerName.exec(playerTwoNameInput.value)
-//   ) {
-//     Game.startGame();
-//   } else {
-//     // error message for valid name
-//     alert(
-//       "Please enter a valid player name. Cannot contain symbols, space or numbers."
-//     );
-//   }
-// };
+restartGameButton.addEventListener("click", () => {
+  Game.restart();
+});
