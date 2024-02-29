1bf3e9b2c35850c6d8a1d0aff1d46188a91856fc
Derek J <derekjiang25@gmail.com>
2024-02-29 13:55:44 +1100

    added basic game functions - start/restart/handling cells

--- javascript-course/basics/tic-tac-toe/personal attempt/tic-tac-toe.js
+++ javascript-course/basics/tic-tac-toe/personal attempt/tic-tac-toe.js
@@ -3,6 +3,8 @@ const restartGameButton = document.querySelector("#restart-game");
 
 const PLAYER_ONE_MARKER = "x";
 const PLAYER_TWO_MARKER = "o";
+const EMPTY_CELL = "";
+const GAMEBOARD_SIZE = 9;
 
 const Gameboard = (() => {
   let gameboard = ["", "", "", "", "", "", "", "", ""];
@@ -13,13 +15,26 @@ const Gameboard = (() => {
       gameboardHTML += `<div class="square" id="square-${index}">${cell}</div>`;
     });
     document.querySelector(".gameboard").innerHTML = gameboardHTML;
+    const cells = document.querySelectorAll(".square");
+    cells.forEach((cell) => {
+      cell.addEventListener("click", Game.handleClick);
+    });
   };
 
-  return { render };
+  const getGameboard = () => gameboard;
+
+  const updateCell = (index, marker) => {
+    gameboard[index] = marker;
+    render();
+  };
+
+  return { render, getGameboard, updateCell };
 })();
 
 const Game = (() => {
   let players = [];
+  let currentPlayerIndex;
+  let gameOver;
 
   const start = () => {
     players = [
@@ -32,11 +47,27 @@ const Game = (() => {
         PLAYER_TWO_MARKER
       ),
     ];
+    currentPlayerIndex = 0;
+    gameOver = false;
 
     Gameboard.render();
   };
 
-  return { start };
+  const restart = () => {
+    for (let i = 0; i < GAMEBOARD_SIZE; i++) {
+      Gameboard.updateCell(i, EMPTY_CELL);
+    }
+  };
+
+  const handleClick = (event) => {
+    let cellIndex = event.target.id.split("-")[1];
+
+    Gameboard.getGameboard()[cellIndex] = players[currentPlayerIndex].marker;
+    Gameboard.render();
+    currentPlayerIndex = currentPlayerIndex == 0 ? 1 : 0;
+  };
+
+  return { start, restart, handleClick };
 })();
 
 const createPlayer = (name, marker) => {
