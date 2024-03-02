918328690054050832c172ce2b9c0ccf84bc92b3
Derek J <derekjiang25@gmail.com>
2024-03-02 14:35:56 +1100

    added library logic

--- javascript-course/basics/object-library/class-version/library-class.js
+++ javascript-course/basics/object-library/class-version/library-class.js
@@ -1,14 +1,45 @@
 const Library = (() => {
   let books = [];
 
-  const addBook = () => {};
+  const addBook = () => {
+    showLibrary();
+    if (validateInput()) {
+      console.log("valid");
+    } else {
+      renderMessage("Input(s) are invalid");
+    }
+  };
 
   const getBooks = () => books;
-  return { addBook, getBooks };
+
+  const showLibrary = () => {
+    if (books.length > 0) {
+      console.log(books);
+    } else {
+      renderMessage("Empty library");
+    }
+  };
+
+  const renderMessage = (message) => {
+    document.querySelector(".message").innerHTML = message;
+  };
+
+  return { addBook, getBooks, showLibrary };
 })();
 
 const Book = (author, title, totalPages, isRead) => {
   return { author, title, totalPages, isRead };
+};
+
+const validateInput = () => {
+  if (
+    document.querySelector("#author").value == "" ||
+    document.querySelector("#title").value == "" ||
+    isNaN(parseInt(document.querySelector("#totalPages").value))
+  ) {
+    return false;
+  }
+  return true;
 };
 
 document.querySelector("#addBook").addEventListener("click", Library.addBook);
