aabc8bc29af1aeef03d597c27716adc44db3db74
Derek J <derekjiang25@gmail.com>
2024-03-02 14:04:51 +1100

    added skeleton html, css & js module for Library

--- javascript-course/basics/object-library/class-version/library-class.js
+++ javascript-course/basics/object-library/class-version/library-class.js
@@ -1,1 +1,14 @@
-console.log("hi");
+const Library = (() => {
+  let books = [];
+
+  const addBook = () => {};
+
+  const getBooks = () => books;
+  return { addBook, getBooks };
+})();
+
+const Book = (author, title, totalPages, isRead) => {
+  return { author, title, totalPages, isRead };
+};
+
+document.querySelector("#addBook").addEventListener("click", Library.addBook);
