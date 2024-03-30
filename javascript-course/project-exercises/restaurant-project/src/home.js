7daec1eac37af40d10200412857e890704f83675
Derek J <derekjiang25@gmail.com>
2024-03-31 10:41:57 +1100

    fixed up homepage

--- javascript-course/project-exercises/restaurant-project/src/home.js
+++ javascript-course/project-exercises/restaurant-project/src/home.js
@@ -1,37 +1,25 @@
 function loadHome() {
   const contentPage = document.querySelector("#content");
-  const restaurantTitleDiv = document.createElement("div");
-  const restaurantTitle = document.createElement("h1");
-  restaurantTitle.innerHTML = "Resturant Title";
-  restaurantTitleDiv.appendChild(restaurantTitle);
-  contentPage.appendChild(restaurantTitleDiv);
+  const homeTitle = document.createElement("h1");
+  homeTitle.innerHTML = "Restaurant Name";
 
-  const contentPageDivOne = document.createElement("div");
-  const restaurantImage = document.createElement("img");
-  const restaurantDescription = document.createElement("div");
-  restaurantDescription.innerHTML = "some temp stuff";
-  contentPageDivOne.append(restaurantImage, restaurantDescription);
-  contentPage.appendChild(contentPageDivOne);
+  const homeDivOne = document.createElement("div");
+  const homeDivOneHeading = document.createElement("h2");
+  homeDivOneHeading.innerHTML = "Location";
+  const homeDivOneContent = document.createElement("p");
+  homeDivOneContent.innerHTML =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nemo laudantium repellat odio non, officiis, atque consequuntur voluptatem fugit facilis vitae! Minima ad magnam iusto. Alias praesentium obcaecati neque expedita?";
+  homeDivOne.append(homeDivOneHeading, homeDivOneContent);
 
-  const contentPageDivTwo = document.createElement("div");
-  const contentOurStoryDiv = document.createElement("div");
-  const contentOurStoryTitle = document.createElement("h1");
-  const contentOurStoryContent = document.createElement("p");
-  contentOurStoryTitle.innerHTML = "Our story";
-  contentOurStoryContent.innerHTML =
-    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi veritatis enim voluptatibus, quis, consequuntur, minima a nisi ratione veniam earum deleniti unde eum consectetur dolorem velit? Consequuntur, culpa commodi? Nulla!";
-  contentOurStoryDiv.append(contentOurStoryTitle, contentOurStoryContent);
-  const contentViewMenu = document.createElement("div");
-  const contentViewMenuTitle = document.createElement("h1");
-  contentViewMenuTitle.innerHTML = "View Menu";
-  contentViewMenu.appendChild(contentViewMenuTitle);
-  const contentAboutUs = document.createElement("div");
-  const contentAboutUsTitle = document.createElement("h1");
-  contentAboutUsTitle.innerHTML = "About us";
-  contentAboutUs.appendChild(contentAboutUsTitle);
+  const homeDivTwo = document.createElement("div");
+  const homeDivTwoHeading = document.createElement("h2");
+  homeDivTwoHeading.innerHTML = "Opening hours";
+  const homeDivTwoContent = document.createElement("p");
+  homeDivTwoContent.innerHTML =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nemo laudantium repellat odio non, officiis, atque consequuntur voluptatem fugit facilis vitae! Minima ad magnam iusto. Alias praesentium obcaecati neque expedita?";
+  homeDivTwo.append(homeDivTwoHeading, homeDivTwoContent);
 
-  contentPageDivTwo.append(contentOurStoryDiv, contentViewMenu, contentAboutUs);
-  contentPage.appendChild(contentPageDivTwo);
+  contentPage.append(homeTitle, homeDivOne, homeDivTwo);
 }
 
 export default loadHome;
