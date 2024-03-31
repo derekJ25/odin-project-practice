6b7c01eab2a73055b8df15541e00fe9c45207fa9
Derek J <derekjiang25@gmail.com>
2024-03-31 11:12:56 +1100

    added logic for tab switching and clearing content child

--- javascript-course/project-exercises/restaurant-project/src/index.js
+++ javascript-course/project-exercises/restaurant-project/src/index.js
@@ -3,6 +3,25 @@ import loadHome from "./home";
 import loadMenu from "./menu";
 import loadAbout from "./about";
 
-// loadHome();
-loadMenu();
-// loadAbout();
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", (button) => {
+    const value = button.target.value;
+    clearContent();
+    if (value == "home") {
+      loadHome();
+    }
+    if (value == "menu") {
+      loadMenu();
+    }
+    if (value == "about") {
+      loadAbout();
+    }
+  });
+});
+
+const clearContent = () => {
+  const content = document.querySelector("#content");
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
+  }
+};
