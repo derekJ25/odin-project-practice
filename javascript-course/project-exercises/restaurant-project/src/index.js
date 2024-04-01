ebc6ef310e340cebde2813e1699b5da91d95ec7b
Derek J <derekjiang25@gmail.com>
2024-04-02 10:14:25 +1100

    added inital page load

--- javascript-course/project-exercises/restaurant-project/src/index.js
+++ javascript-course/project-exercises/restaurant-project/src/index.js
@@ -1,35 +1,4 @@
 // import _ from "lodash";
-import loadHome from "./home";
-import loadMenu from "./menu";
-import loadAbout from "./about";
+import initialPageLoad from "./initialPageLoad";
 
-loadHome();
-
-document.querySelectorAll("button").forEach((button) => {
-  button.addEventListener("click", (button) => {
-    const value = button.target.value;
-    switch (value) {
-      case "home":
-        clearContent();
-        loadHome();
-        break;
-      case "menu":
-        clearContent();
-        loadMenu();
-        break;
-      case "about":
-        clearContent();
-        loadAbout();
-        break;
-      default:
-        loadHome();
-    }
-  });
-});
-
-const clearContent = () => {
-  const content = document.querySelector("#content");
-  while (content.firstChild) {
-    content.removeChild(content.firstChild);
-  }
-};
+initialPageLoad();
---
+++ javascript-course/project-exercises/restaurant-project/src/initialPageLoad.js
@@ -0,0 +1,37 @@
+import loadHome from "./home";
+import loadMenu from "./menu";
+import loadAbout from "./about";
+const initialPageLoad = () => {
+  loadHome();
+
+  document.querySelectorAll("button").forEach((button) => {
+    button.addEventListener("click", (button) => {
+      const value = button.target.value;
+      switch (value) {
+        case "home":
+          clearContent();
+          loadHome();
+          break;
+        case "menu":
+          clearContent();
+          loadMenu();
+          break;
+        case "about":
+          clearContent();
+          loadAbout();
+          break;
+        default:
+          loadHome();
+      }
+    });
+  });
+
+  const clearContent = () => {
+    const content = document.querySelector("#content");
+    while (content.firstChild) {
+      content.removeChild(content.firstChild);
+    }
+  };
+};
+
+export default initialPageLoad;
