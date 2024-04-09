633e79657b3edb43ad9e5bed98b58caeb9baa85e
Derek J <derekjiang25@gmail.com>
2024-04-09 10:15:31 +1000

    added array of testing items

--- javascript-course/project-exercises/todo-list-project/src/index.js
+++ javascript-course/project-exercises/todo-list-project/src/index.js
@@ -1,16 +1,50 @@
-const { default: createToDoItem } = require("./todo-item");
+const { createToDoItem } = require("./todo-item");
 
-const itemOneTitle = "coding practice";
-const itemOneDescription =
-  "do some coding exercises each day or go learn some coding practices";
-const itemOneDueDate = "end of the year";
-const itemOnePriority = "high";
+// Array already creates the class & one version that you have to turn JSON into JS obj
 
-const createItem = createToDoItem(
-  itemOneTitle,
-  itemOneDescription,
-  itemOneDueDate,
-  itemOnePriority
-);
+// const itemsJSON = {
+//   itemOne: {
+//     name: "",
+//     description: "",
+//     dueDate: new Date(2024 - 12 - 31),
+//     priority: "",
+//   },
+//   itemTwo: {
+//     name: "",
+//     description: "",
+//     dueDate: new Date(2024 - 6 - 30),
+//     priority: "",
+//   },
+//   itemThree: {
+//     name: "",
+//     description: "",
+//     dueDate: new Date(2024 - 2 - 29),
+//     priority: "",
+//   },
+// };
 
-createItem.displayItem();
+const itemsCreated = [
+  new createToDoItem(
+    "title1",
+    "this is a test",
+    new Date(2024 - 12 - 31),
+    "high"
+  ),
+  new createToDoItem(
+    "title2",
+    "this is a description",
+    new Date(2024 - 6 - 30),
+    "medium"
+  ),
+  new createToDoItem(
+    "title3",
+    "what are we doing",
+    new Date(2024 - 2 - 29),
+    "low"
+  ),
+];
+// itemsCreated.forEach((item) => {
+//   item.displayItem();
+// });
+
+console.log(itemsCreated[0]);
