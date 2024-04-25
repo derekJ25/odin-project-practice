f0b33729c175dc8c3d7eef5eb2b0de67f6fac674
Derek J <derekjiang25@gmail.com>
2024-04-25 14:41:58 +1000

    added basic creation of list for side bar

--- javascript-course/project-exercises/todo-list-project/src/js/home.js
+++ javascript-course/project-exercises/todo-list-project/src/js/home.js
@@ -1,18 +1,16 @@
 const Home = (data) => {
   const projectContainer = document.querySelector(".projects");
   data.forEach((project) => {
-    console.log(project);
+    const list = document.createElement("ul");
+    const listName = document.createElement("h1");
+    listName.innerHTML = project.name;
+    project.items.forEach((item) => {
+      const listItem = document.createElement("li");
+      listItem.innerHTML = item.title;
+      list.appendChild(listItem);
+    });
+    projectContainer.append(listName, list);
   });
-  // const list = document.createElement("ul");
-  // const listName = document.createElement("h1");
-  //   listName.innerHTML = data.name;
-  //   data.items.forEach((item) => {
-  //     const listItem = document.createElement("li");
-  //     listItem.innerHTML = item.title;
-  //     list.appendChild(listItem);
-  //   });
-
-  //   projectContainer.append(listName, list);
 };
 
 export default Home;
