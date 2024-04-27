34b0dabbc3a663ca5fc3e86c1a2d74a35e84680f
Derek J <derekjiang25@gmail.com>
2024-04-27 16:50:03 +1000

    removed content module temp code

--- javascript-course/project-exercises/todo-list-project/src/js/content.js
+++ javascript-course/project-exercises/todo-list-project/src/js/content.js
@@ -1,15 +1,3 @@
-const Content = (data) => {
-  const contentContainer = document.querySelector(".content");
-  const navbarContainerButtons = document.querySelectorAll("button");
-  navbarContainerButtons.forEach((button) => {
-    if (button.classList.contains("active")) {
-      console.log(button.innerHTML);
-      // Load the content for this button
-      const contentTitle = document.createElement("h1");
-      contentTitle.innerHTML = button.innerHTML;
-      contentContainer.appendChild(contentTitle);
-    }
-  });
-};
+const Content = (data) => {};
 
 export default Content;
