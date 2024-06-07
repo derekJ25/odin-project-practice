289b231b608f3dfe5c9eebcee98b98d45c408aee
Derek J <derekjiang25@gmail.com>
2024-06-07 10:52:40 +1000

    added notes in index.js

--- javascript-course/project-exercises/todo-list-project/src/index.js
+++ javascript-course/project-exercises/todo-list-project/src/index.js
@@ -3,4 +3,7 @@ import "./styles/styles.css";
 
 document.querySelector(".add-list-button").addEventListener("click", () => {
   console.log("add list thing here");
+
+  // basic form popup?
+  // title, date, description, priority.
 });
