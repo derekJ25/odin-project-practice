16b1db6d1d23ac751d4cf45e52500518c1864826
Derek J <derekjiang25@gmail.com>
2024-06-13 11:40:22 +1000

    added function to get date & set tag as todays date

--- javascript-course/project-exercises/todo-list-project/src/index.js
+++ javascript-course/project-exercises/todo-list-project/src/index.js
@@ -3,7 +3,16 @@ import "./styles/styles.css";
 
 document.querySelector(".add-list-button").addEventListener("click", () => {
   console.log("add list thing here");
-
-  // basic form popup?
-  // title, date, description, priority.
 });
+
+const getTodayDate = () => {
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0");
+  const yyyy = today.getFullYear();
+  return `${yyyy}-${mm}-${dd}`;
+};
+
+console.log(getTodayDate());
+
+document.getElementById("dueDate").value = getTodayDate();
