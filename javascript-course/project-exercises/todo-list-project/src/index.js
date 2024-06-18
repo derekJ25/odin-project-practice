6a7b06b2bb2873324364d70827b29b619476ab9c
Derek J <derekjiang25@gmail.com>
2024-06-18 10:34:33 +1000

    added styling & js for form visibility and transition

--- javascript-course/project-exercises/todo-list-project/src/index.js
+++ javascript-course/project-exercises/todo-list-project/src/index.js
@@ -1,21 +1,35 @@
 import "./styles/css-reset.css";
 import "./styles/styles.css";
 
-document.querySelector(".add-list-button").addEventListener("click", () => {
-  console.log("add list thing here");
-});
+const onPageLoad = () => {
+  document.querySelector(".add-list-button").addEventListener("click", () => {
+    console.log("add list thing here");
+    document.querySelector(".add-list").classList.add("active");
+  });
 
-const getTodayDate = () => {
-  const today = new Date();
-  const dd = String(today.getDate()).padStart(2, "0");
-  const mm = String(today.getMonth() + 1).padStart(2, "0");
-  const yyyy = today.getFullYear();
-  return `${yyyy}-${mm}-${dd}`;
+  document.querySelector(".save-button").addEventListener("click", () => {
+    console.log("add the list to the thing");
+  });
+
+  document.querySelector(".cancel-button").addEventListener("click", () => {
+    console.log("go back");
+    document.querySelector(".add-list").classList.remove("active");
+  });
+
+  const getTodayDate = () => {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, "0");
+    const mm = String(today.getMonth() + 1).padStart(2, "0");
+    const yyyy = today.getFullYear();
+    return `${yyyy}-${mm}-${dd}`;
+  };
+
+  const setMinDate = () => {
+    document.getElementById("dueDate").value = getTodayDate();
+    document.getElementById("dueDate").setAttribute("min", getTodayDate());
+  };
+
+  setMinDate();
 };
 
-const setMinDate = () => {
-  document.getElementById("dueDate").value = getTodayDate();
-  document.getElementById("dueDate").setAttribute("min", getTodayDate());
-};
-
-setMinDate();
+onPageLoad();
